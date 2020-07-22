28db6044c704050f7825dd78f8b9a99c947fb976
gaurav-chaurasia <gaurav.181cv155@nitk.edu.in>
2020-07-22 15:26:30 +0530

    complete styleing

--- js/app.js
+++ js/app.js
@@ -1,37 +1,73 @@
-/* all consts will go here at the top */ 
-const cursor = $('.cursor');
-const full_slider = $('.full-slider');
-const slider = $('.slider');
-const span0 = $('.span').eq(0);
-const span1 = $('.span').eq(1);
-const span2 = $('.span').eq(2);
-const hamburger = $('.hamburger');
+/* all consts will go here at the top */
 
-/* cursor animation code starts below*/ 
-$(document).mousemove(function(e) {
-    cursor.css('left', e.pageX + "px");
-    cursor.css('top', e.pageY + "px");
+const cursor = $(".cursor");
+const full_slider = $(".full-slider");
+const slider = $(".slider");
+const span0 = $(".span").eq(0);
+const span1 = $(".span").eq(1);
+const span2 = $(".span").eq(2);
+const hamburger = $(".hamburger");
+
+/* cursor animation code starts below*/
+
+$(document).mousemove(function (e) {
+  cursor.css("left", e.pageX + "px");
+  cursor.css("top", e.pageY + "px");
 });
 
-$('html').on({ // making cursor disappera on leaving screen
-    mouseenter: function () {
-        cursor.toggleClass('cursor-move');
-    },
-    mouseleave: function () {
-        cursor.toggleClass('cursor-move');
-    }
+$("html").on({
+  // making cursor disappera on leaving screen
+  mouseenter: function () {
+    cursor.toggleClass("cursor-move");
+  },
+  mouseleave: function () {
+    cursor.toggleClass("cursor-move");
+  },
 });
 
-$('li a').on({ // making cursor enlarge on hovering links
-    mouseenter: function() {
-        cursor.addClass('animate-cursor');
-        cursor.addClass('cursor-invert');
-    },
-    mouseleave: function () {
-        cursor.removeClass('animate-cursor');
-        cursor.removeClass('cursor-invert');
-    }
+$("li a, .resume, .link-to-projects").on({
+  // making cursor enlarge on hovering links
+  mouseenter: function () {
+    cursor.addClass("animate-cursor");
+    cursor.addClass("cursor-invert");
+  },
+  mouseleave: function () {
+    cursor.removeClass("animate-cursor");
+    cursor.removeClass("cursor-invert");
+  },
 });
+
+
+$("footer").on({
+	// making cursor enlarge on hovering links
+	mouseenter: function () {
+		cursor.addClass("cursor-invert");
+	},
+	mouseleave: function () {
+		cursor.removeClass("cursor-invert");
+	},
+});
+$(".footer-links-link a").on({
+	// making cursor enlarge on hovering links
+	mouseenter: function () {
+		cursor.addClass("animate-cursor");
+	},
+	mouseleave: function () {
+		cursor.removeClass("animate-cursor");
+	},
+});
+
+// $("").on({
+//   // making cursor enlarge on hovering links
+//   mouseenter: function () {
+//     cursor.addClass("animate-cursor");
+//     cursor.addClass("cursor-invert");
+//   },
+//   mouseleave: function () {
+//     cursor.removeClass("animate-cursor");
+//     cursor.removeClass("cursor-invert");
+//   },
+// });
 // $('a').on({ // making cursor enlarge on hovering links
 //     mouseenter: function () {
 //         cursor.addClass('animate-cursor');
@@ -43,61 +79,57 @@ $('li a').on({ // making cursor enlarge on hovering links
 // console.log($("button"));
 // console.log($(".cursor"));
 
-$('.brand').on({ // showing owns img on hover over name
-    mouseenter: function () {
-        cursor.toggleClass('on-logo');
-    },
-    mouseleave: function () {
-        cursor.toggleClass('on-logo');
-    }
+$(".brand").on({
+  // showing owns img on hover over name
+  mouseenter: function () {
+    cursor.toggleClass("on-logo");
+  },
+  mouseleave: function () {
+    cursor.toggleClass("on-logo");
+  },
 });
 
-$('img').on({ 
-    mouseenter: function () {
-        cursor.toggleClass('on-img');
-    },
-    mouseleave: function () {
-        cursor.toggleClass('on-img');
-    }
+$("img").on({
+  mouseenter: function () {
+    cursor.toggleClass("on-img");
+  },
+  mouseleave: function () {
+    cursor.toggleClass("on-img");
+  },
 });
 hamburger.on({
-    mouseenter: function () {
-        cursor.toggleClass('on-cross');
-        // cursor.toggleClass('cursor-invert');
-    },
-    mouseleave: function () {
-        cursor.toggleClass('on-cross');
-        // cursor.toggleClass('cursor-invert');
-    }
-});
-hamburger.click(function () {
-    // document.querySelector('.nav').classList.toggle('nav-toggle');
-    span1.toggleClass('slide');
-    span0.toggleClass('cross-down');
-    span2.toggleClass('cross-up');
-    // console.log($('.span')[1]);
+  mouseenter: function () {
+    cursor.toggleClass("on-cross");
+    // cursor.toggleClass('cursor-invert');
+  },
+  mouseleave: function () {
+    cursor.toggleClass("on-cross");
+    // cursor.toggleClass('cursor-invert');
+  },
 });
 
-hamburger.click(function () {
-    slider.toggleClass('show-slider');
-    full_slider.toggleClass('show-full-slider');
-    $('.project-link').toggleClass('hide');
-    $('.manu-link').toggleClass('hide');
-});
+function showing_slider() {
+  // document.querySelector('.nav').classList.toggle('nav-toggle');
+  span1.toggleClass("slide");
+  span0.toggleClass("cross-down");
+  span2.toggleClass("cross-up");
+  // console.log($('.span')[1]);
+  slider.toggleClass("show-slider");
+  full_slider.toggleClass("show-full-slider");
+  $(".project-link").toggleClass("hide");
+  $(".manu-link").toggleClass("hide");
+}
+hamburger.click(showing_slider);
 
 $(document).mousemove(function () {
-    if ($(".slider:hover").length == 0) {
-        full_slider.click(function () {
-            slider.toggleClass('show-slider');
-            full_slider.toggleClass('show-full-slider');
-            span1.toggleClass('slide');
-            span0.toggleClass('cross-down');
-            span2.toggleClass('cross-up');
-            $('.project-link').toggleClass('hide');
-            $('.manu-link').toggleClass('hide');
-        });
-    }
+  if ($(".slider:hover").length == 0) {
+    full_slider.click(function () {
+      showing_slider();
+    });
+  }
 });
+
+$('.manu-link').click(showing_slider);
 // document.onreadystatechange = function () {
 //     if (document.readyState === "loading") {
 //         document.querySelector("body").style.visibility = "hidden";
@@ -105,4 +137,74 @@ $(document).mousemove(function () {
 //     } else {
 //         document.querySelector("body").style.visibility = "visible";
 //     }
-// }; 
+// };
+
+
+$('.img1').on({
+    mouseenter: function () {
+        $('.img1 .project-title').toggleClass('fade');
+    },
+    mouseleave: function () {
+        $('.img1 .project-title').toggleClass('fade');
+    },
+});
+$('.img2').on({
+    mouseenter: function () {
+        $('.img2 .project-title').toggleClass('fade');
+    },
+    mouseleave: function () {
+        $('.img2 .project-title').toggleClass('fade');
+    },
+});
+$('.img3').on({
+    mouseenter: function () {
+        $('.img3 .project-title').toggleClass('fade');
+    },
+    mouseleave: function () {
+        $('.img3 .project-title').toggleClass('fade');
+    },
+});
+$('.img4').on({
+    mouseenter: function () {
+        $('.img4 .project-title').toggleClass('fade');
+    },
+    mouseleave: function () {
+        $('.img4 .project-title').toggleClass('fade');
+    },
+});
+
+
+function isInViewport(element) {
+	var elementTop = $(element).offset().top;
+	var elementBottom = elementTop + $(element).outerHeight();
+	var viewportTop = $(window).scrollTop();
+	var viewportBottom = viewportTop + $(window).height();
+	return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on( {
+	//Code here
+	scroll: function() {
+		if (isInViewport('.b')) {
+		// console.log('success.')
+		$('footer').css('opacity', '0');
+		} else {
+			//  Remove class
+			// $('.main-page').removeClass('test');
+			// console.log('No success.')
+			$('footer').css('opacity', '1');
+		}
+	}
+});
+
+// $(window).scroll(function () {
+//   if ($('.b').isInViewport()) {
+//     //  Use .blogcard instead of this
+//     // $('.blogcard').addClass('test');
+//     console.log('success.')
+//   } else {
+//     //  Remove class
+//     // $('.main-page').removeClass('test');
+//     console.log('No success.')
+//   }
+// });
