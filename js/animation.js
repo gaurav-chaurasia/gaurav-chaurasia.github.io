30e3724eedf177a4cd65d497040538bcd029b471
gaurav-chaurasia <gaurav.181cv155@nitk.edu.in>
2020-07-23 12:45:40 +0530

    optimaization for mobile devices

--- js/animation.js
+++ js/animation.js
@@ -1,42 +1,103 @@
 AOS.init(); // initializing animate on scroll
 
 $(".blocks").css("visibility", "hidden");
-
 $(window).ready(function () {
   $(".blocks").css("visibility", "visible");
-
+  
+  let windowWidth = $(window).width();
   let tl = anime.timeline({
     easing: "easeOutExpo",
     duration: 750,
   });
 
-  tl.add({
-    targets: ".blocks div",
-    width: "100%",
-    backgroundColor: "rgb(54, 255, 211)",
-    delay: anime.stagger(100), // increase delay by 100ms for each elements.
-  })
-    .add({
+  if (windowWidth >= 1000) {
+      tl.add({
+        targets: ".blocks div",
+        width: "100%",
+        backgroundColor: "rgb(54, 255, 211)",
+        delay: anime.stagger(100), // increase delay by 100ms for each elements.
+      })
+        .add({
+          targets: ".blocks div",
+          width: "90%",
+          backgroundColor: "rgb(235, 235, 235)",
+        })
+        .add(
+          {
+            targets: "body",
+            top: "20%",
+            opacity: 1,
+          },
+          "-=1600"
+        )
+        .add({
+          targets: ".blocks",
+          scale: "2",
+          // scaleX: '2',
+          translateX: "40%",
+          rotate: "45deg",
+          duration: 2000,
+        });
+  } else if (windowWidth >= 700 && windowWidth < 1000) {
+    tl.add({
       targets: ".blocks div",
-      width: "90%",
-      backgroundColor: "rgb(235, 235, 235)",
+      width: "100%",
+      backgroundColor: "rgb(54, 255, 211)",
+      delay: anime.stagger(100), // increase delay by 100ms for each elements.
     })
-    .add(
-      {
-        targets: "body",
-        top: "20%",
-        opacity: 1,
-      },
-      "-=1600"
-    )
-    .add({
-      targets: ".blocks",
-      scale: "2",
-      // scaleX: '2',
-      translateX: "40%",
-      rotate: "45deg",
-      duration: 2000,
-    });
+      .add({
+        targets: ".blocks div",
+        width: "90%",
+        backgroundColor: "rgb(235, 235, 235)",
+      })
+      .add(
+        {
+          targets: "body",
+          top: "20%",
+          opacity: 1,
+        },
+        "-=1600"
+      )
+      .add({
+        targets: ".blocks",
+        scale: "2",
+        // scaleX: '2',
+        translateX: "50%",
+        translateY: "-10%",
+        rotate: "45deg",
+        duration: 2000,
+      });
+  } else {
+    tl.add({
+      targets: ".blocks div",
+      width: "100%",
+      backgroundColor: "rgb(54, 255, 211)",
+      delay: anime.stagger(100), // increase delay by 100ms for each elements.
+    })
+      .add({
+        targets: ".blocks div",
+        width: "90%",
+        backgroundColor: "rgb(235, 235, 235)",
+      })
+      .add(
+        {
+          targets: "body",
+          top: "20%",
+          opacity: 1,
+        },
+        "-=1600"
+      )
+      .add({
+        targets: ".blocks",
+        scale: "2",
+        // scaleX: '2',
+        translateX: "45%",
+        translateY: "20%",
+        rotate: "20deg",
+        duration: 2000,
+      }); 
+  }
+
   $(".main-page").css("animation-play-state", "running");
 });
 
