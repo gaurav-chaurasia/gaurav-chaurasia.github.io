9f95edaadfc02ced8b6108bb87ffd2b637be22e2
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-02-16 17:18:31 +0530

    just triming and removing extra spaces in filter input

--- js/filter.js
+++ js/filter.js
@@ -1,13 +1,18 @@
-const filter = function(input, element) {
-    let filter_input = input.value.toUpperCase();
+const custom_filter = function(input, element) {
+    // getting input value converting all to uppercase 
+    // to compair, trim it 
+    let filter_input = input.value.toUpperCase().trim();
+    
+    // remove extra spaces
+    filter_input = filter_input.replace(/  +/g, ' ');
 
     for (let i = 0; i < element.length; i++) {
         let target = element[i].querySelector('.project-title h4').innerHTML;
-        console.log(target);
 
         if (target.toUpperCase().indexOf(filter_input) > -1) {
             element[i].style.display = '';
-        } else {
+        } 
+        else {
             element[i].style.display = 'none';
         }
     }
